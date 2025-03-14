4f16a2719ff99fb19579cf363c77ac2989e1b4b7
Luisa Fernanda Henao Posada <luisaf.henaop@autonoma.edu.co>
2025-03-13 22:47:42 -0500

    Se agrega la clase perosona

---
+++ src/autonoma/biblioteca/app/models/Persona.java
@@ -0,0 +1,55 @@
+
+package autonoma.biblioteca.app.models;
+
+/**
+ *
+ * @author Luisa Fernanda Henao Posada
+ * @since 20251303
+ * @version 1.0.0
+ */
+public class Persona {
+    
+    // Atributos
+    
+    //Nombre de la persona
+    private String nombre;
+    //Documento de identificacion de la persona
+    private String documentoIdentidad;
+    //Correo electronico de la persona;
+    private String correoElectronico;
+    
+    //Metodo constructor
+    public Persona(String nombre, String documentoIdentidad, String correoElectronico) {
+        this.nombre = nombre;
+        this.documentoIdentidad = documentoIdentidad;
+        this.correoElectronico = correoElectronico;
+    }
+    
+    //Metodos getter y setter
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
+}
