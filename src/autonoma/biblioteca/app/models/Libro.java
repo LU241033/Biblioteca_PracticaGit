c3e322dd6599701d738c42f44d223834d05dc48e
Jacobo Henao <jacobohenao0411@gmail.com>
2025-03-14 09:57:54 -0500

    Se agrego clase Libro

--- src/autonoma/biblioteca/app/models/Libro.java
+++ src/autonoma/biblioteca/app/models/Libro.java
@@ -6,8 +6,42 @@ package autonoma.biblioteca.app.models;
 
 /**
  *
- * @author juanl
+ * @author Juan Jacobo Cañas Henao
+ * @since 20251403
+ * @version 1.0.0
  */
 public class Libro {
+    //Atributos
+    
+    //Numero id del Libro
+    private long id;
+    
+    //titulo del Libro
+    private String titulo;
+    
+    //Metodo constructor
+
+    public Libro(long id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+    
+    //Metodos get and set
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     
 }
