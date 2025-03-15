1a9ede7f1457ff07715f163b2e0187d61bc82828
Jacobo Henao <jacobohenao0411@gmail.com>
2025-03-14 19:21:10 -0500

    Se agrego metodos agg libro y mostrar libro

--- src/autonoma/biblioteca/app/models/Biblioteca.java
+++ src/autonoma/biblioteca/app/models/Biblioteca.java
@@ -7,7 +7,35 @@ package autonoma.biblioteca.app.models;
 /**
  *
  * @author Luisa Fernanda Henao
+ * @since 20252403
+ * @version 1.0.0
  */
+
+import java.util.ArrayList;
 public class Biblioteca {
-    
+    //Se crea la lista para almacenar los libros
+    private ArrayList<Libro> libros;
+    //Metodo constructor de el Array
+    public Biblioteca() {
+        this.libros = new ArrayList<>();
+    }
+    //Metodo para mostrar los libros
+    public String mostrarLibros(){
+        if(libros.isEmpty()){
+            return "La biblioteca no tiene libros";
+        }
+        String resultados = "Libros en la biblioteca\n";
+        for(Libro libro : libros){
+            resultados += "ID: "+libro.getId()+", Titulo: "+libro.getTitulo()+"\n";
+        }
+        
+        return resultados;
+    }
+    //Metodo para agregar libro
+    public boolean agregarLibro(Libro libro){
+        if(libro == null){
+            return false;
+        }
+        return libros.add(libro);
+    }
 }
