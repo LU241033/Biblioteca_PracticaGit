864418a708aa29e7d8a28c656bb9461c82a35897
Luisa Fernanda Henao Posada <luisaf.henaop@autonoma.edu.co>
2025-03-14 20:23:22 -0500

    Se agregaron los metodos Actualizar y BuscarAlfabeticamente

--- src/autonoma/biblioteca/app/main/SistemaBiblioteca.java
+++ src/autonoma/biblioteca/app/main/SistemaBiblioteca.java
@@ -14,27 +14,25 @@ public class SistemaBiblioteca {
 
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca ();
-        Scanner teclado =new Scanner(System.in);
-        
-        System.out.println("Ingrese el ID del libro :");
-        long id= teclado.nextLong();
-        teclado.nextLine();
-        System.out.println("Ingrese el titulo del libro: ");
-        String titulo = teclado.nextLine();
-        
-        Libro nuevoLibro= new Libro(id,titulo);
-        boolean agregado= biblioteca.agregarLibro(nuevoLibro);
-        if(agregado){
-            System.out.println("Libro agregado");
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.println("Ingrese el ID del libro :");
+            long id= teclado.nextLong();
+            teclado.nextLine();
+            System.out.println("Ingrese el titulo del libro: ");
+            String titulo = teclado.nextLine();
             
-        }else{
-            System.out.println("No se pudo agregar el libro");
+            Libro nuevoLibro= new Libro(id,titulo);
+            boolean agregado= biblioteca.agregarLibro(nuevoLibro);
+            if(agregado){
+                System.out.println("Libro agregado");
+                
+            }else{
+                System.out.println("No se pudo agregar el libro");
+            }
+            
+            System.out.println(biblioteca.mostrarLibros());   
         }
         
-        System.out.println(biblioteca.mostrarLibros());
-        
-        teclado.close();
-        
     }
-    
 }
+
