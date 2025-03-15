a4904a52cef5dbeccf0f810776f5a9e7d64d9d85
Jacobo Henao <jacobohenao0411@gmail.com>
2025-03-14 19:35:29 -0500

    Se agrego funcionamiento al main de agg libro ym Libro

--- src/autonoma/biblioteca/app/main/SistemaBiblioteca.java
+++ src/autonoma/biblioteca/app/main/SistemaBiblioteca.java
@@ -1,4 +1,9 @@
 package autonoma.biblioteca.app.main;
+
+import autonoma.biblioteca.app.models.Biblioteca;
+import autonoma.biblioteca.app.models.Libro;
+import java.util.Scanner;
+
 /**
  *
  * @author Luisa Fernanda Henao Posada
@@ -8,6 +13,27 @@ package autonoma.biblioteca.app.main;
 public class SistemaBiblioteca {
 
     public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca ();
+        Scanner teclado =new Scanner(System.in);
+        
+        System.out.println("Ingrese el ID del libro :");
+        long id= teclado.nextLong();
+        teclado.nextLine();
+        System.out.println("Ingrese el titulo del libro: ");
+        String titulo = teclado.nextLine();
+        
+        Libro nuevoLibro= new Libro(id,titulo);
+        boolean agregado= biblioteca.agregarLibro(nuevoLibro);
+        if(agregado){
+            System.out.println("Libro agregado");
+            
+        }else{
+            System.out.println("No se pudo agregar el libro");
+        }
+        
+        System.out.println(biblioteca.mostrarLibros());
+        
+        teclado.close();
         
     }
     
